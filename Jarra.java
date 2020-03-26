95e7d532b4701845b6de56c74ed1759fa79f00cb
PabloGorSan <gordillosanchezp@uma.es>
2020-03-26 11:38:09 +0100

    LlenaDesdeIncluido

--- Jarra.java
+++ Jarra.java
@@ -15,3 +15,13 @@ public class Jarra {
 		System.out.println("Aqui escribimos todos"); 
 	}
 }
+
+public void llenaDesde(Jarra j){
+	while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
+		this.cantidad++;
+		j.cantidad--;
+	}
+}
+public String toString(){
+	return ("("+this.capacidad+","+this.cantidad+")");
+}
