db569b5c303d2fd480c9626e58dd0ea16bcaba4f
CristhianVL <cristhian.vl@uma.es>
2020-03-26 11:41:10 +0100

    Version de Cristhian con llena() y vacia()

--- Jarra.java
+++ Jarra.java
@@ -4,14 +4,22 @@ public class Jarra {
 	private int capacidad; 
 	public Jarra(int capacidadInicial){ 
 		if(capacidadInicial<=0){ 
-			throw new RuntimeException("ERROR: no se pueden crear jarras “ + "con capacidad menor o igual que cero.");
+			throw new RuntimeException("ERROR: no se pueden crear jarras â€œ + "con capacidad menor o igual que cero.");
 
 		}
 		this.capacidad=capacidadInicial;
 	}
-//  Aquí vendrán después los métodos creados en la siguiente fase 
+//  AquÃ­ vendrÃ¡n despuÃ©s los mÃ©todos creados en la siguiente fase 
 	public void metodoComunitario(){ 
 		// Comentario incluido para pruebas 
 		System.out.println("Aqui escribimos todos"); 
+	}
+	
+	public bool llena(){
+		return this.cantidad=this.capacidad;
+	}
+	
+	public bool vacia(){
+		return this.cantidad=0;
 	}
 }
